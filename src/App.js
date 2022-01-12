68abeb0edefac787508bb72fa4aebc241ab51547
Akshay <akshayjoshi4770@gmail.com>
2022-01-13 01:42:09 +0530

    defined getfile function for input - reading and decoding the file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import Dexie from 'dexie';
+import {useState} from 'react';
+
 function App() {
 
     // create an instance of dexie
@@ -10,7 +12,24 @@ function App() {
   db.version(1).stores({
     songs : "song"
   })
+  // error handling
+  db.open().catch((err) => {console.log(err.stack || err)})
+
+ // state variables
   
+
+
+  // reading and decoding  the file using FileReader javascript object
+
+  const getfile = (e) => {
+    
+    let reader = new FileReader();
+    reader.readAsDataURL(e[0]);
+    reader.onload = (e) => {
+      console.log(reader.result);
+    }
+  }
+
   
 
   return (
@@ -18,7 +37,7 @@ function App() {
 
        <h1>Audio Player</h1>
 
-       <input type="file" />
+       <input type="file" name='file' onChange={e => getfile(e.target.files)}/>
 
 
 
